df48fb03f5134caa0e1f0922a8ffa4e4778d98dc
Luiz Otavio <luizomf@gmail.com>
2021-03-15 18:59:08 -0300

    findOne e count

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -9,10 +9,21 @@ module.exports = {
 
   async create(ctx) {
     const { id } = ctx.state.user;
-    const { title, content } = ctx.request.body;
-    const post = { title, content, user: id };
 
-    const entity = await strapi.services.post.create(post);
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.create({
+        ...data,
+        user: id
+      }, { files });
+    } else {
+      entity = await strapi.services.post.create({
+        ...ctx.request.body,
+        user: id
+      });
+    }
+
     return sanitizeEntity(entity, { model: strapi.models.post });
   },
 
@@ -28,5 +39,21 @@ module.exports = {
     }
 
     return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.post }));
+  },
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const entity = await strapi.services.post.findOne({ id, user: ctx.state.user.id });
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+
+  async count(ctx) {
+    const query = { ...ctx.query, user: ctx.state.user.id };
+
+    if (ctx.query._q) {
+      return await strapi.services.post.countSearch(query);
+    };
+
+    return await strapi.services.post.count(query);
   },
 };
