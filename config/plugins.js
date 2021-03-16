aa8eaa41823a728e4178d75f3420a54f9e4ee28a
Luiz Otavio <luizomf@gmail.com>
2021-03-16 10:13:45 -0300

    SMTP configs

--- config/plugins.js
+++ config/plugins.js
@@ -10,5 +10,22 @@ module.exports = ({ env }) => ({
       upload: {},
       delete: {},
     },
-  }
+  },
+  email: {
+    provider: 'smtp',
+    providerOptions: {
+      host: env('SMTP_HOST'), //SMTP Host
+      port: env.int('SMTP_PORT'), //SMTP Port
+      secure: true,
+      username: env('SMTP_USERNAME'),
+      password: env('SMTP_PASSWORD'),
+      rejectUnauthorized: true,
+      requireTLS: true,
+      connectionTimeout: 1,
+    },
+    settings: {
+      from: env('SMTP_FROM'),
+      replyTo: env('SMTP_REPLY_TO'),
+    },
+  },
 });
